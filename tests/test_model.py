6bd978f65b0128cde904cb123da033a11a9725cb
B-Wathek <153394978+B-Wathek@users.noreply.github.com>
2026-03-30 00:46:36 +0300

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,1 +1,12 @@
+from src.model import SpamClassifier
 
+def test_prediction():
+    texts = ["free money", "hello friend"]
+    labels = ["spam", "ham"]
+
+    model = SpamClassifier()
+    model.train(texts, labels)
+
+    result = model.predict(["free money"])
+    assert result[0] == "spam"
+  
