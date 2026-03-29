4d159c197323fb4f419e5dfaaa67e939693fddc7
B-Wathek <153394978+B-Wathek@users.noreply.github.com>
2026-03-30 00:46:05 +0300

    Update train.py

--- pipelines/train.py
+++ pipelines/train.py
@@ -1,1 +1,17 @@
+from src.model import SpamClassifier
 
+# Simple dataset
+texts = [
+    "Win a free iPhone",
+    "Hello how are you",
+    "Claim your prize now",
+    "Let's meet tomorrow"
+]
+
+labels = ["spam", "ham", "spam", "ham"]
+
+model = SpamClassifier()
+model.train(texts, labels)
+
+predictions = model.predict(["Free prize now"])
+print(predictions)
